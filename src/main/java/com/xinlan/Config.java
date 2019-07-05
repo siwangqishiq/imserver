cfecdd571eb9e49ec8fd2dfc6e428399ad4b3d28
yixin.panyi <panyi@yixin.im>
2019-07-05 19:50:54 +0800

    add youpanyun

--- src/main/java/com/xinlan/Config.java
+++ src/main/java/com/xinlan/Config.java
@@ -5,4 +5,7 @@ public final class Config {
 
     public static final long ACCOUNT_TOKEN_EXPIRE_TIME = 24 * 60 * 60 * 1000; // 签发的token超时时间
 
+    public static final String CLOUD_STORAGE_PICPATH = "sqimage";
+    public static final String CLOUD_STORAGE_USER = "luluxiu";
+    public static final String CLOUD_STORAGE_PWD = "M4nx69MqX6L43CNPz03UB8jsvnrgTzP7";
 }
---
+++ src/main/test/com.xinlan/clouds/CloudStorageTest.java
@@ -0,0 +1,35 @@
+package com.xinlan.clouds;
+
+import com.UpYun;
+import com.upyun.FormUploader;
+import com.upyun.UpException;
+import com.xinlan.Config;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class CloudStorageTest {
+    @Test
+    public void testUpload() throws IOException, UpException {
+        UpYun upyun = new UpYun(Config.CLOUD_STORAGE_PICPATH,
+                Config.CLOUD_STORAGE_USER, Config.CLOUD_STORAGE_PWD);
+        upyun.setDebug(true);
+        upyun.setApiDomain(UpYun.ED_AUTO);
+
+        File file = new File("D:\\test.jfif");
+        // 上传文件，并自动创建父级目录（最多10级）
+        boolean result = upyun.writeFile("/sqimage/panyitest", file, true);
+        Assert.assertTrue(result);
+
+        // 图片宽度
+        System.out.println("图片宽度:" + upyun.getPicWidth());
+        // 图片高度
+        System.out.println("图片高度:" + upyun.getPicHeight());
+        // 图片帧数
+        System.out.println("图片帧数:" + upyun.getPicFrames());
+        // 图片类型
+        System.out.println("图片类型:" + upyun.getPicType());
+    }
+}
